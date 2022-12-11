9227f5f0b5d4e2352ee2346870259cad3f8efa1d
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-11 08:26:36 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,125 @@
 input.onButtonPressed(Button.A, function () {
-    Neprestan = 0
-    while (Neprestan == 0) {
-        if (input.lightLevel() > 100) {
-            basic.showLeds(`
-                # # # # #
-                # . . . #
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-        } else if (input.lightLevel() < 100) {
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                . # . # .
-                . # # # .
-                . . . . .
-                `)
-        }
-    }
+    prestan_a = 0
+    prestan_b = 1
 })
 input.onButtonPressed(Button.B, function () {
-    Neprestan = 1
-    while (Neprestan == 1) {
-        if (input.temperature() > 20) {
-            basic.showLeds(`
-                . . . . .
-                . . # . .
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-        } else if (input.temperature() < 15) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . # . .
-                # # # # #
-                `)
+    prestan_b = 0
+    prestan_a = 1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    control.reset()
+})
+let neprestan1 = 0
+let neprestan = 0
+let prestan_b = 0
+let prestan_a = 0
+basic.showArrow(ArrowNames.East)
+basic.showArrow(ArrowNames.West)
+basic.pause(100)
+basic.clearScreen()
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && prestan_a == 0) {
+        neprestan = 0
+        while (neprestan == 0) {
+            if (input.lightLevel() > 100) {
+                basic.showLeds(`
+                    # # # # #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # # # # #
+                    `)
+            } else if (input.lightLevel() < 100) {
+                basic.showLeds(`
+                    . . . . .
+                    . # # # .
+                    . # . # .
+                    . # # # .
+                    . . . . .
+                    `)
+            }
         }
     }
-})
-let Neprestan = 0
-basic.showString("A OR B")
-basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.B) && prestan_b == 0) {
+        prestan_a = 1
+        neprestan = 1
+        while (neprestan == 1) {
+            if (input.temperature() == 50) {
+                neprestan1 = 0
+                while (neprestan1 == 0) {
+                    basic.showLeds(`
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        `)
+                    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1, 4671, 255, 255, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        `)
+                    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 1, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+                }
+            } else {
+                if (input.temperature() == 40) {
+                    neprestan1 = 1
+                    basic.showLeds(`
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        `)
+                } else {
+                    if (input.temperature() == 30) {
+                        neprestan1 = 1
+                        basic.showLeds(`
+                            . . # . .
+                            # # # # #
+                            # # # # #
+                            # # # # #
+                            # # # # #
+                            `)
+                    } else {
+                        if (input.temperature() == 20) {
+                            neprestan1 = 1
+                            basic.showLeds(`
+                                . . . . .
+                                . . # . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                `)
+                        } else {
+                            if (input.temperature() == 15) {
+                                neprestan1 = 1
+                                basic.showLeds(`
+                                    . . . . .
+                                    . . . . .
+                                    . . # . .
+                                    # # # # #
+                                    # # # # #
+                                    `)
+                            } else {
+                                if (input.temperature() == 0) {
+                                    neprestan1 = 1
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        . . . . .
+                                        . . # . .
+                                        # # # # #
+                                        `)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
